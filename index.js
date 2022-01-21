4ef0ac7fb419dcadf739b55f99cd08c5a76c53e6
Boston Rohan <bostonrohan@gmail.com>
2022-01-20 17:32:29 -0800

    added credentials to cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Middleware
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
