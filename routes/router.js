02d723d29a7839f99ad0764846093701660c01ca
Boston Rohan <bostonrohan@gmail.com>
2022-01-28 21:22:53 -0800

    updated quiz routes

--- routes/router.js
+++ routes/router.js
@@ -4,6 +4,8 @@ import createUser from "../controllers/auth/createUser.js";
 import loginUser from "../controllers/auth/loginUser.js";
 import loggedIn from "../controllers/auth/loggedIn.js";
 import logoutUser from "../controllers/auth/logoutUser.js";
+import quiz from "../controllers/quiz/quiz.js";
+import getAns from "../middleware/getAns.js";
 
 const router = express.Router();
 
@@ -12,6 +14,13 @@ router.post("/quizzes/signup", createUser);
 router.post("/quizzes/login", loginUser);
 router.get("/quizzes/", loggedIn);
 router.get("/quizzes/logout", logoutUser);
+
+//quizzes
+router.post(
+  ["/quiz/team", "quiz/nba", "quiz/hard", "quiz/player"],
+  getAns,
+  quiz
+);
 
 //tweets
 router.get("/", getTweets);
