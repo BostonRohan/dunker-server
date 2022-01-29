7f436b1e1cca6e8256cc1dd2384a2e299572b135
Boston Rohan <bostonrohan@gmail.com>
2022-01-28 20:43:36 -0800

    updated user model

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    score: { type: Number, required: false },
+    score: { type: Array, required: false },
   },
   { timestamps: true }
 );
