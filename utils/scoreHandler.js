659c1aad82cab2c11ce174a2107b86c492cb0eac
Boston Rohan <bostonrohan@gmail.com>
2022-01-31 17:01:15 -0800

    turned each string in the array into a num

--- utils/scoreHandler.js
+++ utils/scoreHandler.js
@@ -1,7 +1,7 @@
 const scoreHandler = (ans, selected) => {
   let score = 10;
   for (let i = 0; i < ans.length; i++) {
-    if (selected[i] !== ans[i]) {
+    if (selected[i] !== +ans[i]) {
       score--;
     }
   }
