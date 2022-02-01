31f7794197da7a7cc62621548b4ee98d61b9cef1
Boston Rohan <bostonrohan@gmail.com>
2022-01-31 17:00:22 -0800

    refactored middleware to get data from db

--- middleware/getAns.js
+++ middleware/getAns.js
@@ -1,27 +1,11 @@
-import { nba } from "../utils/ans.js";
-import { team } from "../utils/ans.js";
-import { hard } from "../utils/ans.js";
-import { player } from "../utils/ans.js";
-const getAns = (req, res, next) => {
+import Quiz from "../models/quizModel.js";
+const getAns = async (req, res, next) => {
   const quizName = Object.keys(req.body).join(" ");
-  let ans = [];
-  switch (quizName) {
-    case "nba":
-      ans = nba;
-      break;
-    case "team":
-      ans = team;
-      break;
-    case "hard":
-      ans = hard;
-      break;
-    case "player":
-      ans = player;
-      break;
-    default:
-      ans = [];
-  }
-  req.ans = ans;
+  const data = await Quiz.find({});
+  const ans = data.map((prop) => {
+    return prop[quizName];
+  });
+  req.ans = ans[0];
   next();
 };
 export default getAns;
