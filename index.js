51cb184141907482158658aba182ff232ef43a53
Boston Rohan <bostonrohan@gmail.com>
2022-02-15 19:59:17 -0800

    moved cors

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Middleware
-app.use(express.json());
 app.use(
   cors({
     origin: true,
     credentials: true,
   })
 );
+app.use(express.json());
 app.use(cookieParser());
 
 //Routes
