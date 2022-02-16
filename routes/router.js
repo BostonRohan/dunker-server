5796d36c2581613981b58c80cebc3f4579d0d930
Boston Rohan <bostonrohan@gmail.com>
2022-02-16 08:58:34 -0800

    removed user functionality

--- routes/router.js
+++ routes/router.js
@@ -1,15 +1,9 @@
 import express from "express";
 import getTweets from "../controllers/tweets/tweets.js";
-import createUser from "../controllers/auth/createUser.js";
-import loginUser from "../controllers/auth/loginUser.js";
 import quiz from "../controllers/quiz/quiz.js";
 import getAns from "../middleware/getAns.js";
 
 const router = express.Router();
-
-//auth
-router.post("/quizzes/signup", createUser);
-router.post("/quizzes/login", loginUser);
 
 //quizzes
 router.post(
