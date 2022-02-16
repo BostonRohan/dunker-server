786a5b357a5db1dbdbbaa6bad6ae22785978dab2
Boston Rohan <bostonrohan@gmail.com>
2022-02-15 20:52:20 -0800

    removed logoutroute

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,6 @@ import getTweets from "../controllers/tweets/tweets.js";
 import createUser from "../controllers/auth/createUser.js";
 import loginUser from "../controllers/auth/loginUser.js";
 import loggedIn from "../controllers/auth/loggedIn.js";
-import logoutUser from "../controllers/auth/logoutUser.js";
 import quiz from "../controllers/quiz/quiz.js";
 import getAns from "../middleware/getAns.js";
 
@@ -13,7 +12,6 @@ const router = express.Router();
 router.post("/quizzes/signup", createUser);
 router.post("/quizzes/login", loginUser);
 router.get("/quizzes/", loggedIn);
-router.get("/quizzes/logout", logoutUser);
 
 //quizzes
 router.post(
