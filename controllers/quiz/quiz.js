61b8973a1cf5c25652589fba69b9ea4c3d371c06
Boston Rohan <bostonrohan@gmail.com>
2022-03-27 17:12:28 -0700

    removed user functionality

--- controllers/quiz/quiz.js
+++ controllers/quiz/quiz.js
@@ -1,6 +1,4 @@
 import scoreHandler from "../../utils/scoreHandler.js";
-import User from "../../models/userModel.js";
-import jwt from "jsonwebtoken";
 
 const quiz = async (req, res) => {
   const token = req.cookies.token;
@@ -9,14 +7,6 @@ const quiz = async (req, res) => {
   if (!token) return res.status(401).json({ message: "Unauthorized" });
 
   const score = scoreHandler(req.ans, selected);
-
-  const validatedUser = jwt.verify(token, process.env.JWT_SECRET);
-
-  const user = await User.findOne({ _id: validatedUser.id });
-
-  user.score = [...user.score, score];
-
-  await user.save();
 
   res.status(200).send(score.toString());
 };
