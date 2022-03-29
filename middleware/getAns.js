3b0567bc9eb99260c4ac12bd36f9fe57cc9a6b85
Boston Rohan <bostonrohan@gmail.com>
2022-03-28 17:26:27 -0700

    adjusted quiz handling

--- middleware/getAns.js
+++ middleware/getAns.js
@@ -1,11 +1,10 @@
 import Quiz from "../models/quizModel.js";
+
 const getAns = async (req, res, next) => {
   const quizName = Object.keys(req.body).join(" ");
   const data = await Quiz.find({});
-  const ans = data.map((prop) => {
-    return prop[quizName];
-  });
-  req.ans = ans[0];
+  const ans = data[0][quizName];
+  req.ans = ans;
   next();
 };
 export default getAns;
--- models/quizModel.js
+++ models/quizModel.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
-const quizSchema = new mongoose.Schema(
-  { team: Array, nba: String, player: Array, hard: Array },
-  { collection: "quiz" }
-);
+const quizSchema = new mongoose.Schema({
+  team: Array,
+  nba: String,
+  player: Array,
+  hard: Array,
+});
 
-const Quiz = mongoose.model("Quiz", quizSchema);
+const Quiz = mongoose.model("Quiz", quizSchema, "quiz");
 
 export default Quiz;
